1c58e56e373458f173233785afb8cf71e58203e7
leonardomeneguetti <lrsmeneguetti@gmail.com>
2024-03-22 11:53:38 -0300

    correções finais

--- ExercicioFixacao4/app/src/main/java/br/edu/ifsp/arq/ads/dmos5/ex4/MainActivity.kt
+++ ExercicioFixacao4/app/src/main/java/br/edu/ifsp/arq/ads/dmos5/ex4/MainActivity.kt
@@ -29,12 +29,12 @@ class MainActivity : AppCompatActivity() {
     fun onClickBtnSend(view: View?) {
         val intent = Intent(this, MainActivity2::class.java)
 
-        val nome = edtNome.toString()
-        val email = edtEmail.toString()
-        val telefone = edtTelefone.toString()
-        val cpf = edtCpf.toString()
-        val endereco = edtEndereco.toString()
-        val cidade = edtCidade.toString()
+        val nome = edtNome.text.toString()
+        val email = edtEmail.text.toString()
+        val telefone = edtTelefone.text.toString()
+        val cpf = edtCpf.text.toString()
+        val endereco = edtEndereco.text.toString()
+        val cidade = edtCidade.text.toString()
 
         val cliente = Cliente(nome, email, telefone, cpf, endereco, cidade)
         intent.putExtra("cliente", cliente)
